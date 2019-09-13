a5be75f27ec0117b2065d8764ed98bd506a198bc
GaveenJanitha <gaveenjanitha007@gmail.com>
2019-09-13 21:30:19 +0530

    Created Card Layout Created Cart Items Layout

--- app/src/main/java/com/example/surge/Cart.java
+++ app/src/main/java/com/example/surge/Cart.java
@@ -1,14 +1,37 @@
 package com.example.surge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Cart extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private Button btnCheckout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        recyclerView = findViewById(R.id.cart_recyclerview);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        btnCheckout = (Button) findViewById(R.id.button_cart_checkout);
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
     }
 }
