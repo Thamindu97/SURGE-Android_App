30ec5f7438f1fff551e1e5e621d969b8ad788eea
Thamindu97 <thamindusasitha8@gmail.com>
2019-09-15 04:22:14 +0530

    All Done v 0.1

--- app/src/main/java/com/example/surge/SignUp.java
+++ app/src/main/java/com/example/surge/SignUp.java
@@ -3,6 +3,7 @@ package com.example.surge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
