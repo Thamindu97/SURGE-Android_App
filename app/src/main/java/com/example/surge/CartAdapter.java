a22e7af282e67fc780553540ca694e708b1f73ee
GaveenJanitha <gaveenjanitha007@gmail.com>
2019-09-14 20:05:07 +0530

    Cart itens

---
+++ app/src/main/java/com/example/surge/CartAdapter.java
@@ -0,0 +1,17 @@
+package com.example.surge;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CartAdapter {
+
+    class CartViewHolder extends RecyclerView.ViewHolder
+    {
+
+        public CartViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
