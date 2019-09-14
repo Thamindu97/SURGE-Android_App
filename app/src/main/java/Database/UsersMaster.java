5494fd694b44946509b5cd167f0661d891f47d0f
GaveenJanitha <gaveenjanitha007@gmail.com>
2019-09-14 10:30:14 +0530

    Card Details - minor updates

--- app/src/main/java/Database/UsersMaster.java
+++ app/src/main/java/Database/UsersMaster.java
@@ -16,7 +16,15 @@ public class UsersMaster implements BaseColumns {
     public static final String COLUMN1_NAME_MOBILENO = "mobileno";
 
 
+    // Card Details Table
+    public static class CardDetails implements BaseColumns {
 
+        public static final String TABLE_NAME = "carddetails";
+        public static final String COLUMN_NAME_USERNAME = "username";
+        public static final String COLUMN_NAME_CARDNO = "cardno";
+        public static final String COLUMN_NAME_EXDATE= "exdate";
+        public static final String COLUMN_NAME_CVV= "cvv";
+    }
 
 
 }
--- app/src/main/java/com/example/surge/MainActivity.java
+++ app/src/main/java/com/example/surge/MainActivity.java
@@ -9,16 +9,26 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCard = (Button) findViewById(R.id.button_addcard);
 
     }
 
     public void onClickSignUp(View view)
     {
         Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+
+    public void onClickaddCard(View view)
+    {
+        Intent intent = new Intent(this, CardDetails.class);
         startActivity(intent);
     }
 }
