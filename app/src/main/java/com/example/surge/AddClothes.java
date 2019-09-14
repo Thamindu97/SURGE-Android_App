b2f09762d657495f94dab1e182735b003b194861
ashfaqsahal <ashfaq.sahal1998@gmail.com>
2019-09-14 10:36:24 +0530

    Retrieve from Database Implemented

--- app/src/main/java/com/example/surge/AddClothes.java
+++ app/src/main/java/com/example/surge/AddClothes.java
@@ -2,6 +2,7 @@ package com.example.surge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,17 +39,26 @@ public class AddClothes extends AppCompatActivity {
 
     public  void AddData() {
         addcloth.setOnClickListener(
+
+
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         boolean isInserted = db.addClothes(clothtype.getText().toString(),
                                 size.getText().toString(),colour.getText().toString(),price.getText().toString());
-                        if(isInserted == true)
-                            Toast.makeText(AddClothes.this,"Data Inserted",Toast.LENGTH_LONG).show();
-                        else
+                        if(isInserted == true) {
+                            Toast.makeText(AddClothes.this, "Data Inserted", Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(AddClothes.this, ClothesView.class);
+                            startActivity(intent);
+                        }else
                             Toast.makeText(AddClothes.this,"Data not Inserted",Toast.LENGTH_LONG).show();
                     }
                 }
+
+
         );
+
+
     }
 }
---
+++ app/src/main/java/com/example/surge/ClothesView.java
@@ -0,0 +1,48 @@
+package com.example.surge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.List;
+
+import Database.DBHandler_AddClothes;
+
+public class ClothesView extends AppCompatActivity {
+
+    ImageView back;
+    DBHandler_AddClothes dbHandler;
+    List<Clothes> clothesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothes_view);
+
+        back = (ImageView)findViewById(R.id.button_back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it1 = new Intent(ClothesView.this,Add.class);
+                startActivity(it1);
+            }
+        });
+
+        ListView listView = (ListView)findViewById(R.id.clothes_list_view);
+
+        dbHandler = new DBHandler_AddClothes(this);
+
+        clothesList = dbHandler.readAllClothes();
+
+        AddsAdapter stocksAdapter = new AddsAdapter(this,R.layout.adapter_clothes_view,clothesList);
+        listView.setAdapter(stocksAdapter);
+
+
+
+    }
+}
