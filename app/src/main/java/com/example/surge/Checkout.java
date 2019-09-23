c7197c3a716b9b1fff1c90b9c98b616c86f74814
GaveenJanitha <gaveenjanitha007@gmail.com>
2019-09-24 02:11:57 +0530

    Card Details Delete Done

--- app/src/main/java/com/example/surge/Checkout.java
+++ app/src/main/java/com/example/surge/Checkout.java
@@ -3,15 +3,35 @@ package com.example.surge;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import Database.DBHandler;
+
 public class Checkout extends AppCompatActivity {
+
+    DBHandler db;
+
+    Button delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        db = new DBHandler(this);
+
+        delete = findViewById(R.id.button_checkout_delete);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.deleteCardDetails();
+                Toast.makeText(Checkout.this,"Card Details Deleted Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void onClickUpdate(View view)
